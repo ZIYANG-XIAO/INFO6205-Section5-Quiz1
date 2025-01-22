6965d77746cc073e45861eda7b1dd49c6e51a2d6
Shawn <xiaoziyang@ShawndeMacBook-Pro.local>
2025-01-22 16:25:36 -0500

    quiz

--- py/InsertionSort.py
+++ py/InsertionSort.py
@@ -14,6 +14,12 @@ class InsertionSort:
     def insert(self, i):
         """Inserts the 'Transition element' into its correct position in the sorted portion of the array."""
     #     TODO: TO BE IMPLEMENTED
+        current_value = self.array[i]
+        position = i
+        while position > 0 and self.array[position - 1] > current_value:
+            self.array[position] = self.array[position - 1]
+            position -= 1
+        self.array[position] = current_value
 
     def swap(self, i, j):
         self.array[i], self.array[j] = self.array[j], self.array[i]
